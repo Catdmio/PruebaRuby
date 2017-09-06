113c389d50a63c287da9ab95d954fd0ce23de5bb
Catdmio <montecinos.catalina@gmail.com>
2017-09-06 00:08:52 -0300

    adding while sentence, these are the options of this app

---
+++ PruebaRuby.rb
@@ -0,0 +1,9 @@
+puts '¡Bienvenido a la API de Gestión de Alumnos! Selecciona la función que deseas realizar:'
+
+options = 0
+while b != 4
+  puts 'Escoje una opción ingresando el número correspondiente'
+  '1. Ingresar nombre de alumno y el promedio de sus notas'
+  '2. Revisar inasistencias totales'
+  '3. Alumnos que actualmente están aprobando el ramo'
+  '4. Salir'
