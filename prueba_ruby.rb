1f360f0f52c868fc75134f82d22131b5e9a74360
Catdmio <montecinos.catalina@gmail.com>
2017-09-06 20:12:04 -0300

    modified some minor ident issues and syntax for one of the methods. Also, modified the while sentence -option menu- to end the app when selecting salir

--- prueba_ruby.rb
+++ prueba_ruby.rb
@@ -1,60 +1,57 @@
-
-#Abre documento txt
+# Abre documento txt
 def lista_notas
-  lines = File.open('Alumnosnotas.txt','r').readlines
+  lines = File.open('Alumnosnotas.txt', 'r').readlines
   new_lines = []
   lines.each { |line| new_lines.push(line.split(', ').map(&:chomp)) }
   return new_lines
 end
 
-#Metodo de inasistencias
+# Metodo de inasistencias
 def inasistencias_totales
   unatend = lista_notas
   unatend.each do |lista_nomnot|
     nombre = lista_nomnot[0]
     total_inasist = lista_nomnot.select { |dataalum| dataalum == 'A' }.length
-    puts 'Nombre: #{nombre} tiene #{total_inasist} inasistencias >:(!!'
+    puts "#{nombre} tiene #{total_inasist} inasistencias >:(!!"
   end
 end
 
-
-
-#Método promedio aprobados
+# Metodo promedio aprobados
 def prom_aprobados
   unatend = lista_notas
   unatend.each do |lista_nomnot|
     nombre = lista_nomnot[0]
-    total_inasist = lista_nomnot.map { |n| n > 0 }
+    total_inasist = lista_nomnot.map { |num| num.to_i > 5 }
     if true
-      puts 'Aprobado'
-    else puts 'cuek, vas a tener que hacer el ramo de nuevo'
+      puts "#{nombre} Aprobado"
+    else puts "#{nombre} cuek, vas a tener que hacer el ramo de nuevo"
     end
   end
 end
 
-#Método de hacer promedios
+# Metodo de hacer promedios
 def promedios
   listapromedios = {}
   lista_nomnot = lista_notas
   lista_nomnot.each do |elem|
     nombre = elem[0]
-    notas = elem.select { |numero| elem.find_index(numero) != 0 &&  numero != 'A' }
+    notas = elem.select { |num| elem.find_index(num) != 0 && num != 'A' }
     notas = notas.map(&:to_i)
-    promedio = notas.sum/notas.count
+    promedio = notas.sum / notas.count
     listapromedios[nombre] = promedio
   end
   escribir_archivo(listapromedios)
 end
 
 def escribir_archivo(listapromedios)
-  File.open( 'listaactualizada.txt','w') do |file|
+  File.open('listaactualizada.txt', 'w') do |file|
     listapromedios.each do |nombre, promedio|
       file.puts "#{nombre} con promedio #{promedio}"
     end
   end
 end
 
-#Método de verificación de aprobación
+# Metodo de verificacion de aprobacion
 def verificar_aprobado(promedio, nombre)
   if promedio > 5
     puts "¡Felicidades! #{nombre} estás pasando el ramo"
@@ -63,29 +60,27 @@ def verificar_aprobado(promedio, nombre)
   end
 end
 
+# Opciones que el usuario podra escoger
+puts '¡Bienvenido a la API de Gestión de Alumnos! Presione enter'
 
-
-#Opciones que el usuario podrá escoger
-puts '¡Bienvenido a la API de Gestión de Alumnos! Selecciona la función que deseas realizar:'
-
-options = 0
-while options != 5
+options = gets.chomp
+while options != '4'
   puts 'Escoje una opción ingresando el número correspondiente'
   puts '1. Alumnos y promedios'
   puts '2. Inasistencias por alumno'
   puts '3. Alumnos aprobados'
   puts '4. Salir'
 
-  option = gets.chomp.to_i
-
-  case option
-  when 1
+  options = gets.chomp
+  case options
+  when '1'
     promedios
-  when 2
+    puts 'Revisa el documento actualizado'
+  when '2'
     inasistencias_totales
-  when 3
+  when '3'
     prom_aprobados
-  when 4
+  when '4'
     puts '¡Hasta luego!'
   else
     puts '¡Mlem! escoja una opción correcta ingresando un número del 1 al 4'
